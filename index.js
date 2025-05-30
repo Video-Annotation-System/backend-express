e3f6b3d03f15cbd20d7118c5f17d52c5a26861df
JabadeSusheelKrishna <susheelkrishna95@gmail.com>
2025-05-30 23:00:17 +0530

    Added routes to GET and POST methods for storing and providing the annotations data

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("hello world");
+// server.js
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// In-memory annotations array
+let annotations = [];
+
+// GET /
+app.get('/', (req, res) => {
+    res.send("Hello world");
+});
+
+// GET /api/annotations
+app.get('/api/annotations', (req, res) => {
+    console.log("received GET to /api/annotations");
+    res.json(annotations);
+});
+
+// POST /api/annotations
+app.post('/api/annotations', (req, res) => {
+    const annotation = req.body;
+    annotations.push(annotation);
+    res.status(201).json({ message: 'Annotation saved' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Annotation backend server running on http://localhost:${PORT}`);
+});
